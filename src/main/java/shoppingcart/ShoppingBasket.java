87727fc3f425284ebed71a1ed39ab7ff7d86e38b
Arturo Menendez <arturo.menendez@codurance.com>
2023-06-06 18:07:52 +0200

    pass failing test

---
+++ src/main/java/shoppingcart/ShoppingBasket.java
@@ -0,0 +1,21 @@
+package shoppingcart;
+
+
+import java.util.ArrayList;
+
+public class ShoppingBasket {
+    private final ArrayList<Product> products;
+
+    public ShoppingBasket() {
+        products = new ArrayList<>();
+    }
+
+    public ShoppingBasket(ArrayList<Product> products) {
+        this.products = products;
+    }
+
+    public void store(Product productToStore) {
+        products.add(productToStore);
+    }
+
+}
